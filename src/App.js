c9981f05e9197d2873fd611b28ed76b8f54feceb
rreckonerr <rreckonerr@gmail.com>
2019-04-22 16:23:17 +0300

    init

--- src/App.js
+++ src/App.js
@@ -1,28 +1,137 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import PropTypes from "prop-types";
+import { connect } from "react-redux";
+import { downloadPosts } from "./redux/reducer";
+import Post from "./redux/components/post";
+
+import classNames from "classnames";
+import { withStyles } from "@material-ui/core/styles";
+import withRoot from "./withRoot";
+import Button from "@material-ui/core/Button";
+
+import AppBar from "@material-ui/core/AppBar";
+import Toolbar from "@material-ui/core/Toolbar";
+import Typography from "@material-ui/core/Typography";
+import IconButton from "@material-ui/core/IconButton";
+import MenuIcon from "@material-ui/icons/Menu";
+import Grid from "@material-ui/core/Grid";
+
+const styles = theme => ({
+  root: {
+    flexGrow: 1
+  },
+  menuButton: {
+    marginLeft: -18,
+    marginRight: 10
+  },
+  postsTitle: {
+    backgroundColor: theme.palette.background.paper
+  },
+  titleContent: {
+    maxWidth: 600,
+    margin: "0 auto",
+    padding: `${theme.spacing.unit * 8}px 0 ${theme.spacing.unit * 6}px`
+  },
+  layout: {
+    width: "auto",
+    marginLeft: theme.spacing.unit * 3,
+    marginRight: theme.spacing.unit * 3,
+    [theme.breakpoints.up(1100 + theme.spacing.unit * 3 * 2)]: {
+      width: 1100,
+      marginLeft: "auto",
+      marginRight: "auto"
+    }
+  },
+  cardGrid: {
+    padding: `${theme.spacing.unit * 8}px 0`
+  }
+});
 
 class App extends Component {
   render() {
+    const { posts, classes } = this.props;
+
+    console.log(this.props);
+
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+      <>
+        <DenseAppBar {...this.props} />
+        <main className="App">
+          <div className={classes.postsTitle}>
+            <div className={classes.titleContent}>
+              <Typography
+                component="h1"
+                variant="h2"
+                align="center"
+                color="textPrimary"
+                gutterBottom
+              >
+                Posts
+              </Typography>
+
+              <h2>Click download button</h2>
+
+              <Button
+                variant="contained"
+                color="primary"
+                onClick={this.props.downloadPosts}
+              >
+                Download
+              </Button>
+            </div>
+          </div>
+          <div className={classNames(classes.layout, classes.cardGrid)}>
+            <Grid container spacing={40}>
+              {posts &&
+                posts.map(post => (
+                  <Post
+                    key={post.id}
+                    post={post}
+                    viewPost={() => {
+                      console.log("--whatsup");
+                    }}
+                    editPost={() => {
+                      console.log("--whatsup");
+                    }}
+                  />
+                ))}
+            </Grid>
+          </div>
+        </main>
+      </>
     );
   }
 }
 
-export default App;
+function DenseAppBar(props) {
+  const { classes } = props;
+  return (
+    <div className={classes.root}>
+      <AppBar position="static">
+        <Toolbar variant="dense">
+          <IconButton
+            className={classes.menuButton}
+            color="inherit"
+            aria-label="Menu"
+          >
+            <MenuIcon />
+          </IconButton>
+          <Typography variant="h6" color="inherit">
+            Demo
+          </Typography>
+        </Toolbar>
+      </AppBar>
+    </div>
+  );
+}
+
+DenseAppBar.propTypes = {
+  classes: PropTypes.object.isRequired
+};
+
+export default connect(
+  state => ({ posts: state.posts }),
+  dispatch => ({
+    downloadPosts: () => dispatch(downloadPosts())
+  })
+)(withRoot(withStyles(styles)(App)));
---
+++ src/redux/reducer.js
@@ -0,0 +1,66 @@
+const defaultStore = {
+  posts: []
+};
+
+const DOWNLOAD_POSTS_START = "DOWNLOAD_POSTS_START";
+const DOWNLOAD_POSTS_SUCCESS = "DOWNLOAD_POSTS_SUCCESS";
+const DOWNLOAD_POSTS_ERROR = "DOWNLOAD_POSTS_ERROR";
+
+export default (state = defaultStore, action) => {
+  const { payload, type } = action;
+
+  switch (type) {
+    case DOWNLOAD_POSTS_START:
+      return {
+        ...state
+      };
+    case DOWNLOAD_POSTS_SUCCESS:
+      return {
+        ...state,
+        posts: payload.posts
+      };
+    case DOWNLOAD_POSTS_ERROR: {
+      return {
+        ...state,
+        error: payload
+      };
+    }
+    default:
+      return state;
+  }
+};
+
+export const downloadPosts = () => dispatch => {
+  dispatch(downloadPostsStarted());
+
+  fetch("https://jsonplaceholder.typicode.com/posts")
+    .then(res => res.json())
+    .then(posts => {
+      console.log("---fetch-res", posts);
+      dispatch(downloadPostsSuccess(posts.slice(0, 10)));
+    })
+    .catch(err => {
+      console.error("error", err);
+      dispatch(downloadPostsError(err));
+    });
+};
+
+const downloadPostsStarted = () => {
+  return {
+    type: DOWNLOAD_POSTS_START
+  };
+};
+
+const downloadPostsSuccess = posts => {
+  return {
+    type: DOWNLOAD_POSTS_SUCCESS,
+    payload: { posts }
+  };
+};
+
+const downloadPostsError = error => {
+  return {
+    type: DOWNLOAD_POSTS_ERROR,
+    peyload: error
+  };
+};
